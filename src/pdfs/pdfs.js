5bb211c1bf5c3e9764cb6d89c3b9ba1e8d54c3a8
carlyleec <carlyleec@gmail.com>
2017-04-24 15:26:03 -0600

    Added Webpack 2, changed webpack.config and the way images, favicon stuff, and pdfs were being loaded, and added some better npm scripts for building

--- src/pdfs/pdfs.js
+++ src/pdfs/pdfs.js
@@ -1,9 +1,3 @@
-// https://medium.com/tech-angels-publications/bundle-your-favicons-with-webpack-b69d834b2f53
-// Thanks Oliver Olivier Gonzalez!
-
-const pdfContext = require.context(
-  '!!file?name=[name].[ext]!.',
-  true,
-  /\.(pdf)$/
-);
-pdfContext.keys().forEach(pdfContext);
+/* eslint-disable */
+require('file-loader?name=[name].[ext]?!./cameron-carlyle-resume.pdf');
+/* eslint-enable */
